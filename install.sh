a4d992a9da61be18ff6ac1cfe1b021df9e66851c
step66 <red.mailhub@gmail.com>
2017-04-08 22:19:49 +0800

    silent
    silent

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
 
-sudo apt-get update
+sudo apt-get update -yqq
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-sudo apt-get update
+sudo apt-get update -yqq
 apt-cache policy docker-engine
-sudo apt-get install -y docker-engine
+sudo apt-get install -yq docker-engine
 sudo systemctl status docker
